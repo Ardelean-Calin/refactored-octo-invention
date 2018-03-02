36632aba5da42e1be27739e04eb7500086003b82
Ardelean Calin <ardelean.calin@outlook.com>
2018-03-02 21:30:29 +0200

    Removed firebase configuration from repository. Still using firebase though, don't worry.

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyDjp913p_pIYDB9bPhF-5RDBymrEbc2lpA",
-  authDomain: "refactored-octo-invention-505a.firebaseapp.com",
-  databaseURL: "https://refactored-octo-invention-505a.firebaseio.com",
-  projectId: "refactored-octo-invention-505a",
-  storageBucket: "refactored-octo-invention-505a.appspot.com",
-  messagingSenderId: "344922511997"
-};

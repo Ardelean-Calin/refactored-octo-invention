54ceba9dcc45600471bb0188ba5bd8a8e20eb8d1
Ardelean Calin <ardelean.calin@outlook.com>
2018-03-04 09:56:59 +0200

    Moved Navigation Bar to separate component.

--- src/components/AppShell.vue
+++ src/components/AppShell.vue
@@ -1,40 +1,6 @@
 <template>
   <div>
-    <v-navigation-drawer 
-      v-model="drawer"
-      app 
-    >
-      <v-divider/>
-      <v-list>
-        <template v-for="navlink in navigationLinks">
-          <v-list-tile 
-            :to="{ path: navlink.route }" 
-            :key="navlink.id">
-            <v-list-tile-action>
-              <v-icon>{{ navlink.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ navlink.name }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Deconectează-te</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar 
-      app 
-      color="primary">
-      <v-toolbar-side-icon 
-        class="white--text" 
-        @click="drawer = !drawer"/>
-    </v-toolbar>
+    <navigation-bar/>
     <v-content>
       <router-view/>
     </v-content>
@@ -42,28 +8,11 @@
 </template>
 
 <script>
+import NavigationBar from "./NavigationBar";
+
 export default {
-  data: function() {
-    return {
-      drawer: null,
-      navigationLinks: [
-        {
-          name: "Acasă",
-          route: "/",
-          icon: "home"
-        },
-        {
-          name: "Listă Discipline",
-          route: "/subjects",
-          icon: "book"
-        },
-        {
-          name: "Orar",
-          route: "/schedule",
-          icon: "date_range"
-        }
-      ]
-    };
+  components: {
+    NavigationBar
   }
 };
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <v-navigation-drawer 
+      v-model="drawer"
+      app 
+    >
+      <v-divider/>
+      <v-list>
+        <template v-for="navlink in navigationLinks">
+          <v-list-tile 
+            :to="{ path: navlink.route }" 
+            :key="navlink.id">
+            <v-list-tile-action>
+              <v-icon>{{ navlink.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ navlink.name }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Deconectează-te</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar 
+      app 
+      color="primary">
+      <v-toolbar-side-icon 
+        class="white--text" 
+        @click="drawer = !drawer"/>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      drawer: null,
+      navigationLinks: [
+        {
+          name: "Acasă",
+          route: "/",
+          icon: "home"
+        },
+        {
+          name: "Listă Discipline",
+          route: "/subjects",
+          icon: "book"
+        },
+        {
+          name: "Orar",
+          route: "/schedule",
+          icon: "date_range"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>

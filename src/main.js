8aa66f559b6c2576133483f06887bdabddd5c3f1
Ardelean Calin <ardelean.calin@outlook.com>
2018-03-07 21:40:37 +0200

    Fixed weird scrolling behavior.

--- src/main.js
+++ src/main.js
@@ -70,7 +70,14 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes: routes
+  routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 
 firebase.auth().onAuthStateChanged(function(user) {
